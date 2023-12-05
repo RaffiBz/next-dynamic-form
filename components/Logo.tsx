7dca5bc1d85b52a0413383332a168cb9367f18bc
Raffi Bzdigian <raffibzdigian98@gmail.com>
2023-12-05 21:45:14 +0400

    added all form builder logic

---
+++ components/Logo.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+function Logo() {
+  return (
+    <Link
+      href={"/"}
+      className="font-bold text-3xl bg-gradient-to-r from-indigo-400 to-cyan-400 text-transparent bg-clip-text hover:cursor-pointer"
+    >
+      <div className="px-auto relative z-30 h-[35px] w-[140px] lg:h-[57px] lg:w-[245px]">
+        <Image alt="logo" src="/procedars.png" fill />
+      </div>
+    </Link>
+  );
+}
+
+export default Logo;
